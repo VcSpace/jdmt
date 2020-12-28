d3014696ac5363e203b74169037dcae2155e4761
Vcvc <vcvckw@gmail.com>
2020-12-28 18:32:50 +0800

    server_time

---
+++ Tool/check_time.py
@@ -0,0 +1,18 @@
+import time
+import requests
+import json
+from datetime import datetime
+
+if __name__ == '__main__':
+    url = 'https://a.jd.com//ajax/queryServerData.html'
+    ret = requests.get(url).text
+    js = json.loads(ret)
+    jd_time = int(js["serverTime"])
+
+    local_time = int(round(time.time() * 1000))
+
+    local_jd_time_diff = local_time - jd_time
+    print(local_time)
+    print(jd_time)
+
+    print("检测本地时间与京东服务器时间误差为{}毫秒".format(local_jd_time_diff))
