11dba9026ee411f9e9cc8dfa31cf1bd0c0bc4159
Vcvc <vcvckw@gmail.com>
2020-12-31 20:43:09 +0800

    jd

--- jd_spider_requests.py
+++ jd_spider_requests.py
@@ -5,30 +5,268 @@ import time
 import requests
 import functools
 import pickle
+import json
 from jd_logger import logger
 from timer import Timer
-from util import parse_json, send_wechat, get_session, get_sku_title, get_random_useragent, send_mail
+from util import (parse_json, get_session, get_sku_title, get_random_useragent, send_mail,
+                        response_status, save_image, open_image, add_bg_for_qr)
 from config import global_config
 from concurrent.futures import ProcessPoolExecutor
 
-"""
-class SpiderSession(object):
+class SpiderSession:
+    """
+    Session相关操作
+    """
     def __init__(self):
-        pass
+        self.cookies_dir_path = "./cookies/"
+        self.user_agent = get_random_useragent()
 
-class QrLogin(object):
-    def __init__(self):
-        pass
-"""
+        self.session = self._init_session()
+
+    def _init_session(self):
+        session = requests.session()
+        session.headers = self.get_headers()
+        return session
+
+    def get_headers(self):
+        return {"User-Agent": self.user_agent,
+                "Accept": "text/html,application/xhtml+xml,application/xml;"
+                          "q=0.9,image/webp,image/apng,*/*;"
+                          "q=0.8,application/signed-exchange;"
+                          "v=b3",
+                "Connection": "keep-alive"}
+
+    def get_user_agent(self):
+        return self.user_agent
+
+    def get_session(self):
+        """
+        获取当前Session
+        :return:
+        """
+        return self.session
+
+    def get_cookies(self):
+        """
+        获取当前Cookies
+        :return:
+        """
+        return self.get_session().cookies
+
+    def set_cookies(self, cookies):
+        self.session.cookies.update(cookies)
+
+    def load_cookies_from_local(self):
+        """
+        从本地加载Cookie
+        :return:
+        """
+        cookies_file = ''
+        if not os.path.exists(self.cookies_dir_path):
+            return False
+        for name in os.listdir(self.cookies_dir_path):
+            if name.endswith(".cookies"):
+                cookies_file = '{}{}'.format(self.cookies_dir_path, name)
+                break
+        if cookies_file == '':
+            return False
+        with open(cookies_file, 'rb') as f:
+            local_cookies = pickle.load(f)
+        self.set_cookies(local_cookies)
+
+    def save_cookies_to_local(self, cookie_file_name):
+        """
+        保存Cookie到本地
+        :param cookie_file_name: 存放Cookie的文件名称
+        :return:
+        """
+        cookies_file = '{}{}.cookies'.format(self.cookies_dir_path, cookie_file_name)
+        directory = os.path.dirname(cookies_file)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(cookies_file, 'wb') as f:
+            pickle.dump(self.get_cookies(), f)
+
+
+class QrLogin:
+    """
+    扫码登录
+    """
+    def __init__(self, spider_session: SpiderSession):
+        """
+        初始化扫码登录
+        大致流程：
+            1、访问登录二维码页面，获取Token
+            2、使用Token获取票据
+            3、校验票据
+        :param spider_session:
+        """
+        self.qrcode_img_file = 'qr_code.png'
+
+        self.spider_session = spider_session
+        self.session = self.spider_session.get_session()
+
+        self.is_login = False
+        self.refresh_login_status()
+
+    def refresh_login_status(self):
+        """
+        刷新是否登录状态
+        :return:
+        """
+        self.is_login = self._validate_cookies()
+
+    def _validate_cookies(self):
+        """
+        验证cookies是否有效（是否登陆）
+        通过访问用户订单列表页进行判断：若未登录，将会重定向到登陆页面。
+        :return: cookies是否有效 True/False
+        """
+        url = 'https://order.jd.com/center/list.action'
+        payload = {
+            'rid': str(int(time.time() * 1000)),
+        }
+        try:
+            resp = self.session.get(url=url, params=payload, allow_redirects=False)
+            if resp.status_code == requests.codes.OK:
+                return True
+        except Exception as e:
+            logger.error("验证cookies是否有效发生异常", e)
+        return False
+
+    def _get_login_page(self):
+        """
+        获取PC端登录页面
+        :return:
+        """
+        url = "https://passport.jd.com/new/login.aspx"
+        page = self.session.get(url, headers=self.spider_session.get_headers())
+        return page
+
+    def _get_qrcode(self):
+        """
+        缓存并展示登录二维码
+        :return:
+        """
+        url = 'https://qr.m.jd.com/show'
+        payload = {
+            'appid': 133,
+            'size': 300,
+            't': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': self.spider_session.get_user_agent(),
+            'Referer': 'https://passport.jd.com/new/login.aspx',
+        }
+        resp = self.session.get(url=url, headers=headers, params=payload)
+
+        if not response_status(resp):
+            logger.info('获取二维码失败')
+            return False
+
+        save_image(resp, self.qrcode_img_file)
+        logger.info('二维码获取成功，请打开京东APP扫描')
+
+        open_image(add_bg_for_qr(self.qrcode_img_file))
+        return True
+
+    def _get_qrcode_ticket(self):
+        """
+        通过 token 获取票据
+        :return:
+        """
+        url = 'https://qr.m.jd.com/check'
+        payload = {
+            'appid': '133',
+            'callback': 'jQuery{}'.format(random.randint(1000000, 9999999)),
+            'token': self.session.cookies.get('wlfstk_smdl'),
+            '_': str(int(time.time() * 1000)),
+        }
+        headers = {
+            'User-Agent': self.spider_session.get_user_agent(),
+            'Referer': 'https://passport.jd.com/new/login.aspx',
+        }
+        resp = self.session.get(url=url, headers=headers, params=payload)
+
+        if not response_status(resp):
+            logger.error('获取二维码扫描结果异常')
+            return False
+
+        resp_json = parse_json(resp.text)
+        if resp_json['code'] != 200:
+            logger.info('Code: %s, Message: %s', resp_json['code'], resp_json['msg'])
+            return None
+        else:
+            logger.info('已完成手机客户端确认')
+            return resp_json['ticket']
+
+    def _validate_qrcode_ticket(self, ticket):
+        """
+        通过已获取的票据进行校验
+        :param ticket: 已获取的票据
+        :return:
+        """
+        url = 'https://passport.jd.com/uc/qrCodeTicketValidation'
+        headers = {
+            'User-Agent': self.spider_session.get_user_agent(),
+            'Referer': 'https://passport.jd.com/uc/login?ltype=logout',
+        }
+
+        resp = self.session.get(url=url, headers=headers, params={'t': ticket})
+        if not response_status(resp):
+            return False
+
+        resp_json = json.loads(resp.text)
+        if resp_json['returnCode'] == 0:
+            return True
+        else:
+            logger.info(resp_json)
+            return False
+
+    def login_by_qrcode(self):
+        """
+        二维码登陆
+        :return:
+        """
+        self._get_login_page()
+
+        # download QR code
+        if not self._get_qrcode():
+            logger.info('二维码下载失败')
+
+        # get QR code ticket
+        ticket = None
+        retry_times = 85
+        for _ in range(retry_times):
+            ticket = self._get_qrcode_ticket()
+            if ticket:
+                break
+            time.sleep(2)
+        else:
+            logger.info('二维码过期，请重新获取扫描')
+
+        # validate QR code ticket
+        if not self._validate_qrcode_ticket(ticket):
+            logger.info('二维码信息校验失败')
+
+        self.refresh_login_status()
+
+        logger.info('二维码登录成功')
+
 
 class JdSeckill(object):
     def __init__(self):
         # 初始化信息
-        #self.spider_session = SpiderSession()
-        #self.spider_session.load_cookies_from_local()
-        #self.qrlogin = QrLogin(self.spider_session)
+        self.spider_session = SpiderSession()
+        self.spider_session.load_cookies_from_local()
+        self.qrlogin = QrLogin(self.spider_session)
 
+        self.session = self.spider_session.get_session()
+        self.user_agent = self.spider_session.user_agent
+        self.nick_name = None
+        """
         self.session = get_session()
+        """
         self.sku_id = global_config.getRaw('config', 'sku_id')
         self.seckill_num = 2 #抢几瓶
         self.seckill_init_info = dict()
@@ -38,6 +276,36 @@ class JdSeckill(object):
         self.timers = Timer()
         self.default_user_agent = get_random_useragent()
 
+    def login_by_qrcode(self):
+        """
+        二维码登陆
+        :return:
+        """
+        if self.qrlogin.is_login:
+            logger.info('登录成功')
+            return
+
+        self.qrlogin.login_by_qrcode()
+
+        if self.qrlogin.is_login:
+            self.nick_name = self.get_username()
+            self.spider_session.save_cookies_to_local(self.nick_name)
+        else:
+            logger.info("二维码登录失败！")
+
+    def check_login(func):
+        """
+        用户登陆态校验装饰器。若用户未登陆，则调用扫码登陆
+        """
+        @functools.wraps(func)
+        def new_func(self, *args, **kwargs):
+            if not self.qrlogin.is_login:
+                logger.info("{0} 需登陆后调用，开始扫码登陆".format(func.__name__))
+                self.login_by_qrcode()
+            return func(self, *args, **kwargs)
+        return new_func
+
+    @check_login
     def reserve(self):
         """
         预约
@@ -218,6 +486,8 @@ class JdSeckill(object):
                 time_end = time.time()  # 结束计时
                 self.sum_t = (time_end - time_start) + self.sum_t  # 运行所花时间
                 self.wait_time()
+
+        return False
 
     def request_seckill_url(self):
         """访问商品的抢购链接（用于设置cookie等"""
